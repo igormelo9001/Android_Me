4a74b6382e170644eb7095268011326b6e5cfdd2
igormelo9001 <igormelo9001@gmail.com>
2018-08-07 23:30:49 -0300

    Created Fragments and Display

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,56 @@
+package com.example.android.android_me.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+import com.example.android.android_me.data.AndroidImageAssets;
+
+import java.util.List;
+
+public class BodyPartFragment extends Fragment {
+
+    private static final String TAG = "BodyPartFragment";
+    private List<Integer> mImageIds;
+    private int mListIndex;
+
+    public BodyPartFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+
+        if (mImageIds != null){
+            imageView.setImageResource(mImageIds.get(mListIndex));
+        }else {
+            Log.v(TAG,"this fragment has a null list of image id's");
+        }
+
+        return rootView;
+    }
+
+    public List<Integer> getmImageIds() {
+        return mImageIds;
+    }
+
+    public void setmImageIds(List<Integer> mImageIds) {
+        this.mImageIds = mImageIds;
+    }
+
+    public int getmListIndex() {
+        return mListIndex;
+    }
+
+    public void setmListIndex(int mListIndex) {
+        this.mListIndex = mListIndex;
+    }
+}
